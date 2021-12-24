6d90cc816843198f5a3defd7f1ec548cfbb1a1f3
Suhail <suhail@mightyapp.com>
2021-12-24 13:37:25 -0800

    Update setup_server_basics.sh

--- setup_server_basics.sh
+++ setup_server_basics.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt-get install apt-file -y
 sudo apt-file update -y
 sudo apt-get -y upgrade
+sudo apt install zsh -y
 sudo apt-get install -y git
 sudo apt-get install -y python-setuptools
 sudo apt-get install -y build-essential
@@ -13,3 +14,6 @@ pip install --upgrade pip
 sudo apt-get install -y cmake
 sudo update-alternatives --config editor
 sudo apt install ncdu
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+sudo sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
