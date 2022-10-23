e05452e80e51d9e8db4775b40ac1a6b01d613d6f
Suhail <suhail@mightyapp.com>
2022-10-23 14:29:58 -0700

    Update setup_server_basics.sh

--- setup_server_basics.sh
+++ setup_server_basics.sh
@@ -15,7 +15,7 @@ pip install --upgrade pip
 sudo apt-get install -y cmake
 sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim && sudo update-alternatives --set editor /usr/local/bin/vim
 sudo apt install ncdu
+sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 source ~/.zshrc
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
