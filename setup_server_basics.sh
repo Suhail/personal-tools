cf0fa1085f5a655da7dc22bd744b35548040a9fe
Suhail <suhail@mightyapp.com>
2022-10-23 14:13:36 -0700

    Update setup_server_basics.sh

--- setup_server_basics.sh
+++ setup_server_basics.sh
@@ -13,7 +13,7 @@ sudo apt install htop -y
 sudo apt install -y python-pip
 pip install --upgrade pip
 sudo apt-get install -y cmake
-sudo update-alternatives --config editor
+sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim && sudo update-alternatives --set editor /usr/local/bin/vim
 sudo apt install ncdu
 source ~/.zshrc
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
