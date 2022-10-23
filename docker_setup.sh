133a9d7c786ff935d40ebf13557aef3d7bc58018
Suhail <suhail@mightyapp.com>
2022-10-23 14:16:17 -0700

    Update docker_setup.sh

--- docker_setup.sh
+++ docker_setup.sh
@@ -1,5 +1,6 @@
-sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get update
+sudo apt-get -y upgrade
+sudo apt-get remove docker docker-engine docker.io containerd runc
 sudo apt-get install \
     ca-certificates \
     curl \
