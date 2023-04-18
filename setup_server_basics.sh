32b024d6757d957c7cba79b523abf280c86a04ba
Suhail <suhail@mightyapp.com>
2023-04-18 10:25:41 -0700

    Update setup_server_basics.sh

--- setup_server_basics.sh
+++ setup_server_basics.sh
@@ -3,7 +3,6 @@ wget https://raw.githubusercontent.com/Suhail/personal-tools/master/.vimrc
 wget https://raw.githubusercontent.com/Suhail/personal-tools/master/.gitconfig
 wget https://raw.githubusercontent.com/Suhail/personal-tools/master/.tmux.conf
 tmux source ~/.tmux.conf
-sudo apt-get update
 sudo apt-get install apt-file -y
 sudo apt-file update -y
 sudo apt-get -y upgrade
@@ -13,6 +12,7 @@ sudo apt-get install -y python-setuptools
 sudo apt-get install -y build-essential
 sudo apt install htop -y
 sudo apt install -y python-pip
+sudo apt install tree
 pip install --upgrade pip
 sudo apt-get install -y cmake
 sudo update-alternatives --install /usr/bin/editor editor /usr/local/bin/vim && sudo update-alternatives --set editor /usr/local/bin/vim
